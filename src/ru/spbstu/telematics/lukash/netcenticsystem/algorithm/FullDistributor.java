0b145d0e39b0c4492046b2df858a3d24df5dad87
Alexey A. Lukashin <alexey@lukash.spb.ru>
2012-08-26 15:51:16 +0400

    compile errors removed

--- src/ru/spbstu/telematics/lukash/netcenticsystem/algorithm/FullDistributor.java
+++ src/ru/spbstu/telematics/lukash/netcenticsystem/algorithm/FullDistributor.java
@@ -1,11 +1,7 @@
 package ru.spbstu.telematics.lukash.netcenticsystem.algorithm;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.SortedSet;
 
-import ru.spbstu.telematics.lukash.netcenticsystem.model.Environment;
 import ru.spbstu.telematics.lukash.netcenticsystem.model.Firewall;
 import ru.spbstu.telematics.lukash.netcenticsystem.model.TVC;
 
@@ -17,13 +13,7 @@ public class FullDistributor extends ConnectionDistributor {
   @Override
   public double distribute(TVC newCon) {
     try {
-      this.originalSet = new HashSet<>();
-      TVC[] arr = copyArray(connectionsSortedUp.toArray(new TVC[connectionsSortedUp.size()]));
-      Firewall[] fws = copyFirewalls(arr, FullDistributor.firewalls);
-      originalSet.addAll(Arrays.asList(arr));
-      
-      return walkThrough(arr, fws);
-      
+      return walkThrough(null, null);
     } catch (Exception e) {
       throw new RuntimeException("fatal error", e);
     }
@@ -40,42 +30,42 @@ public class FullDistributor extends ConnectionDistributor {
       
       TVC[] subset = copySetWithout(c, connections);
       if (num > 1) {
-        Firewall[] fwCopy = copyFirewalls(subset, firewalls);
-        optimalDispersion = Math.min(walkThrough(subset, fwCopy), optimalDispersion);
+//        Firewall[] fwCopy = copyFirewalls(subset, firewalls);
+        optimalDispersion = Math.min(walkThrough(subset, firewalls), optimalDispersion);
       }
       
       //right branch
       optimalDispersion = Math.min(optimalDispersion, getDispersionForConnection(c, false, firewalls));
       
       if (num > 1) {
-        Firewall[] fwCopy = copyFirewalls(subset, firewalls);
-        optimalDispersion = Math.min(walkThrough(subset, fwCopy), optimalDispersion);
+//        Firewall[] fwCopy = copyFirewalls(subset, firewalls);
+        optimalDispersion = Math.min(walkThrough(subset, firewalls), optimalDispersion);
       }
     }
     
     return optimalDispersion;
   }
   
-  private Firewall[] copyFirewalls(TVC[] subset, Firewall[] original) throws CloneNotSupportedException {
-    Firewall[] newCopy = new Firewall[original.length];
-    Set<Integer> ids = new HashSet<>();
-    for (int i = 0; i < original.length; i++) {
-      newCopy[i] = new Firewall(i);
-    }
-    //these connections are cloned and will changed
-    for (TVC s : subset) {
-      newCopy[s.getManagerFirewallId()].manageConnection((TVC) s.clone());
-      ids.add(s.getId());
-    }
-
-    // these connections are from already checked subset
-    for (TVC s : originalSet) {
-      if (!ids.contains(s.getId())) {
-        newCopy[s.getManagerFirewallId()].manageConnection(s);
-      }
-    }
-    return newCopy;
-  }
+//  private Firewall[] copyFirewalls(TVC[] subset, Firewall[] original) throws CloneNotSupportedException {
+//    Firewall[] newCopy = new Firewall[original.length];
+//    Set<Integer> ids = new HashSet<>();
+//    for (int i = 0; i < original.length; i++) {
+//      newCopy[i] = new Firewall(i);
+//    }
+//    //these connections are cloned and will changed
+//    for (TVC s : subset) {
+//      newCopy[s.getManagerFirewallId()].manageConnection((TVC) s.clone());
+//      ids.add(s.getId());
+//    }
+//
+//    // these connections are from already checked subset
+//    for (TVC s : originalSet) {
+//      if (!ids.contains(s.getId())) {
+//        newCopy[s.getManagerFirewallId()].manageConnection(s);
+//      }
+//    }
+//    return newCopy;
+//  }
 
   private TVC[] copySetWithout(TVC withoutIt, TVC[] connections) throws CloneNotSupportedException {
     TVC[] r = new TVC[connections.length - 1];
@@ -97,10 +87,11 @@ public class FullDistributor extends ConnectionDistributor {
     return r;
   }
 
+  //TODO
   private double getDispersionForConnection(TVC con, boolean trueForIdx1, Firewall[] firewalls) {
     int id = trueForIdx1 ? con.getFirewallId1() : con.getFirewallId2();
     firewalls[id].manageConnection(con, firewalls, false);
-    return Environment.dispersion(firewalls);
+    return environment.dispersion();
   }
 
   @Override
